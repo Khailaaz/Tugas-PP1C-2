0cc67217006c1bbd0e4b3b4f96faf4800cea5496
Khailaaz <khailaazharaputri@gmail.com>
2024-10-05 12:57:34 +0700

    Add files via upload

---
+++ khaila 2_1.cpp
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main()
+{
+	int alas = 8;
+	int tinggi = 5;
+	printf("Luas segitiga dengan ukuran alas 8 cm dan tinggi 5cm adalah %d cm", hasil);
+	return 0;
+}
---
+++ khaila 2_3.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <math.h>
+
+int main ()
+{
+	float alas = 4.0, tinggi = 5.0, miring;
+	miring = sqrt((alas * alas ) + (tinggi * tinggi));
+	printf("panjang sisi miring segitiga adalah: %.2f cm", miring);
+	
+	return 0;
+}
